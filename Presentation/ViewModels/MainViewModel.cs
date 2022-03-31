fe43a03eab4253e86faf2a557ad02b37935057cd
Jeppe Walther Jensen <jepwaljen@gmail.com>
2022-03-31 16:53:35 +0200

    Add project files.

---
+++ Presentation/ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.ViewModels
+{
+   class MainViewModel : ViewModelBase
+   {
+      public EditingViewModel EditingVM;
+      public HomeViewModel HomeVM;
+      private object _currentView;
+
+      public object CurrentView
+      {
+         get { return _currentView; }
+         set { _currentView = value; }
+      }
+
+      public MainViewModel()
+      {
+         EditingVM = new EditingViewModel();
+         HomeVM = new HomeViewModel();
+         CurrentView = HomeVM;
+      }
+   }
+}
