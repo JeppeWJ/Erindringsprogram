1a95956079c7ea6cebf285c887e8ba8c81a18d04
Emil Hertz Degn <emilhd@protonmail.com>
2022-04-05 11:53:19 +0200

    using System.Windows.Media.Imaging;

--- DataAccess 2.0/DataAccessGeneric.cs
+++ DataAccess 2.0/DataAccessGeneric.cs
@@ -9,6 +9,7 @@ using Microsoft.Win32;
 using System.Data.SqlClient;
 using System.IO;
 using System.Media;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace DataAccess_2._0
