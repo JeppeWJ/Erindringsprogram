ee3fe5dfcce0ec45a82675048c091e881c147c29
Emil Hertz Degn <emilhd@protonmail.com>
2022-04-14 15:43:44 +0200

    Better methods in DataAccess

--- DataAccess 2.0/DataAccess.cs
+++ DataAccess 2.0/DataAccess.cs
@@ -11,20 +11,25 @@ using System.IO;
 using System.Media;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Drawing;
 
 namespace DataAccess_2
 {
 
     public class DataAccess : DataAccessSubject
     {
-
+        private string connString = "Server=tcp:st4prj4.database.windows.net,1433;Initial Catalog=ST4PRJ4;Persist Security Info=False;User ID=azureuser;Password=Katrinebjerg123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"; //Connection string for the database
         private string imagePath { get; set; }
         public string audioPath { get; set; }
         private SqlDataReader dataReader;
         private byte[] output;
-        private MediaPlayer mediaPlayer = new MediaPlayer();
+        //private MediaPlayer mediaPlayer = new MediaPlayer();
 
-
+        public static byte[] ImageToByte(BitmapImage img)
+        {
+            ImageConverter converter = new ImageConverter();
+            return (byte[])converter.ConvertTo(img, typeof(byte[]));
+        }
 
 
         public void ManageFile(byte[] blob, bool fileType, uint personID)
@@ -32,8 +37,9 @@ namespace DataAccess_2
             NotifyObservers(blob, fileType, personID);
         }
 
-        private void LoadImage() //Click on the load button
+        private void LoadImage(bool fileType, uint personID) //Click on the load button
         {
+            BitmapImage image;
             OpenFileDialog openFile = new OpenFileDialog(); //create object for Filedialog
 
             openFile.FileName = "";
@@ -43,104 +49,27 @@ namespace DataAccess_2
             if (openFile.ShowDialog() == true) //Opens a dialog from the computer and if OK is clicked, the code in the if-statement will be executed
             {
                 imagePath = openFile.FileName; // Set the filename path for the image
-                AddImage.Source = new BitmapImage(new Uri(openFile.FileName)); //Adding and showing the image to the image control in the WPF
+                image = new BitmapImage(new Uri(openFile.FileName)); //Adding and showing the image to the image control in the WPF
+                var imageBA = ImageToByte(image);
+                ManageFile(imageBA, fileType, personID);
             }
+            
+           
         }
+        
+  
 
-
-
-        private void AddImage() //Click on the add button
+        private void UploadImageOrAudio(bool fileType, uint personID) // Upload the sound to the database
         {
-            // Connection string to the database
-            string connString = "Server=tcp:st4prj4.database.windows.net,1433;Initial Catalog=ST4PRJ4;Persist Security Info=False;User ID=azureuser;Password=Katrinebjerg123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"; //Connection string for the database
-
-            SqlConnection connection = new SqlConnection(connString);
-
-            connection.Open(); //Open the connection to the database
-
-            SqlCommand command = connection.CreateCommand();
-
-            // Convert image to byte array
-            byte[] data;
-
-            data = File.ReadAllBytes(imagePath); //Reads the content of the audio file and converting it to byte array
-
-            //Define image parameter
-            command.Parameters.AddWithValue("@image", data);
-
-            //query
-            command.CommandText = "INSERT INTO Test_table(PersonID, FirstName, LastName, Image) VALUES(4, 'Magnus', 'Andersen', @image)"; //Insert commandtext to database
-
-            if (command.ExecuteNonQuery() > 0) //Check if the command get executed to the database
+            string filetype;
+            if (fileType == true)
             {
-                MessageBox.Show("Billedet blev uploadet til database");
+                filetype = "Sound";
             }
             else
             {
-                MessageBox.Show("Billedet blev ikke uploadet til database");
+                filetype = "Image";
             }
-            connection.Close(); // Close connection to the database
-
-        }
-
-        private void ShowImage()
-        {
-            string connString = "Server=tcp:st4prj4.database.windows.net,1433;Initial Catalog=ST4PRJ4;Persist Security Info=False;User ID=azureuser;Password=Katrinebjerg123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"; //Connection string for the database
-
-            SqlConnection connection = new SqlConnection(connString);
-
-            connection.Open();
-
-            SqlCommand command = connection.CreateCommand();
-
-            command.CommandText = "Select Image from Test_table WHERE PersonID=4";
-
-            dataReader = command.ExecuteReader();
-
-
-            while (dataReader.Read())
-            {
-                output = (byte[])dataReader.GetValue(0);
-            }
-
-            connection.Close();
-
-            Stream streamObj = new MemoryStream(output);
-            BitmapImage bitObj = new BitmapImage();
-
-            bitObj.BeginInit();
-            bitObj.StreamSource = streamObj;
-            bitObj.EndInit();
-
-            ShowImage.Source = bitObj;
-
-            //https://stackoverflow.com/questions/14337071/convert-array-of-bytes-to-bitmapimage
-            //https://sqltutorialtips.blogspot.com/2016/11/image-vs-varbinarymax.html
-            //https://www.c-sharpcorner.com/UploadFile/0f100d/storing-and-retrieving-image-from-sql-server-database-in-wpf/
-            //https://www.youtube.com/watch?v=XMhcF-zex6k
-            //https://www.youtube.com/watch?v=HIv1_P98Ne8&list=LL&index=2&t=182s
-        }
-
-        private void AudioPlay() // Chose the audio file from the computer
-        {
-            OpenFileDialog openFile = new OpenFileDialog(); //create object for Filedialog
-
-            openFile.FileName = "";
-
-            openFile.Filter = "MP3 files|*.mp3"; // Filter only supported mp3 files
-
-            if (openFile.ShowDialog() == true) //Opens a dialog from the computer and if OK is clicked, the code in the if-statement will be executed
-            {
-                audioPath = openFile.FileName; // Set the filename path for the audio
-                mediaPlayer.Open(new Uri(audioPath)); //Open the audio from the filepath with the mediaplayer
-                mediaPlayer.Play(); // Play the audio with mediaplayer
-            }
-        }
-
-        private void UploadAudio() // Upload the sound to the database
-        {
-            // Connection string to the database
-            string connString = "Server=tcp:st4prj4.database.windows.net,1433;Initial Catalog=ST4PRJ4;Persist Security Info=False;User ID=azureuser;Password=Katrinebjerg123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"; //Connection string for the database
 
             SqlConnection connection = new SqlConnection(connString);
 
@@ -149,42 +78,64 @@ namespace DataAccess_2
             SqlCommand command = connection.CreateCommand();
 
             // Convert sound to byte array 
-            byte[] data;
+            byte[] data = File.ReadAllBytes(audioPath); //Reads the content of the audio file and converting it to byte array
 
-            data = File.ReadAllBytes(audioPath); //Reads the content of the audio file and converting it to byte array
-
-            //Define sound parameter
-            command.Parameters.AddWithValue("@sound", data);
 
             //query
-            command.CommandText = "INSERT INTO Test_table(PersonID, Sound) VALUES(5, @sound)"; //Insert commandtext to database
+            command.CommandText = $"UPDATE Test_table SET({filetype} = {data}) WHERE(PersonID = {personID})"; //Insert commandtext to database
 
-            if (command.ExecuteNonQuery() > 0) //Check if the command get executed to the database
+            if (fileType)
             {
-                MessageBox.Show("Lyden blev uploadet til database");
+                if (command.ExecuteNonQuery() > 0) //Check if the command get executed to the database
+                {
+                    MessageBox.Show("Billedet blev uploadet til database");
+                }
+                else
+                {
+                    MessageBox.Show("Billedet blev ikke uploadet til database");
+                }
             }
             else
             {
-                MessageBox.Show("Lyden blev ikke uploadet til database");
+                if (command.ExecuteNonQuery() > 0) //Check if the command get executed to the database
+                {
+                    MessageBox.Show("Lyden blev uploadet til database");
+                }
+                else
+                {
+                    MessageBox.Show("Lyden blev ikke uploadet til database");
+                }
             }
+
+            
+
             connection.Close(); // Close the connection from the database
 
         }
 
-        private void GetAudio() // Receive the sound from database
+
+
+        private string GetImageOrAudio(bool fileType, uint personID) // Receive the sound from database
         {
-            // Connection string to the database
-            string connString = "Server=tcp:st4prj4.database.windows.net,1433;Initial Catalog=ST4PRJ4;Persist Security Info=False;User ID=azureuser;Password=Katrinebjerg123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"; //Connection string for the database
+            string filetype; 
+            if (fileType == true)
+            {
+                filetype = "Sound";
+            }
+            else
+            {
+                filetype = "Image";
+            }
+            
 
             SqlConnection connection = new SqlConnection(connString);
-
             connection.Open(); // Open connection to database
-
             SqlCommand command = connection.CreateCommand();
+            command.CommandText = $"Select {filetype} from Test_table WHERE PersonID={personID}"; // Getting the sound from PersonID 5
 
-            command.CommandText = "Select Sound from Test_table WHERE PersonID=5"; // Getting the sound from PersonID 5
 
             dataReader = command.ExecuteReader(); // Execute the command text to the database
+
 
             while (dataReader.Read()) //Reading from the database and converting the data to byte array
             {
@@ -196,10 +147,15 @@ namespace DataAccess_2
             var path = @"file.mp3"; // Specifying a file name
             File.WriteAllBytes(path, output); // Save the filename and byte array in a mp3 file
 
-            string url = @"C:\Users\Søren Mehlsen\source\repos\soerenmehlsen\ST4PRJ4_Database_WPF\ST4PRJ4_Database_WPF\bin\Debug\file.mp3"; //file path
-            mediaPlayer.Open(new Uri(url)); // Open the music file from the path
+            string url = @".\bin\Debug\file.mp3";
 
-            mediaPlayer.Play(); //Play the sound
+            ManageFile(output, fileType, personID);
+
+            return path;
+
+            //@"C:\Users\Søren Mehlsen\source\repos\soerenmehlsen\ST4PRJ4_Database_WPF\ST4PRJ4_Database_WPF\bin\Debug\file.mp3"; //file path
+            //mediaPlayer.Open(new Uri(url)); // Open the music file from the path
+            //mediaPlayer.Play(); //Play the sound
 
             //https://stackoverflow.com/questions/2665362/convert-byte-array-to-wav-file
         }
@@ -210,6 +166,92 @@ namespace DataAccess_2
 
         }
 
+        //private void ShowImage()
+        //{
+        //    string connString = "Server=tcp:st4prj4.database.windows.net,1433;Initial Catalog=ST4PRJ4;Persist Security Info=False;User ID=azureuser;Password=Katrinebjerg123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"; //Connection string for the database
+
+        //    SqlConnection connection = new SqlConnection(connString);
+
+        //    connection.Open();
+
+        //    SqlCommand command = connection.CreateCommand();
+
+        //    command.CommandText = "Select Image from Test_table WHERE PersonID=4";
+
+        //    dataReader = command.ExecuteReader();
+
+
+        //    while (dataReader.Read())
+        //    {
+        //        output = (byte[])dataReader.GetValue(0);
+        //    }
+
+        //    connection.Close();
+
+        //    Stream streamObj = new MemoryStream(output);
+        //    BitmapImage bitObj = new BitmapImage();
+
+        //    bitObj.BeginInit();
+        //    bitObj.StreamSource = streamObj;
+        //    bitObj.EndInit();
+
+        //    ShowImage.Source = bitObj;
+
+        //    //https://stackoverflow.com/questions/14337071/convert-array-of-bytes-to-bitmapimage
+        //    //https://sqltutorialtips.blogspot.com/2016/11/image-vs-varbinarymax.html
+        //    //https://www.c-sharpcorner.com/UploadFile/0f100d/storing-and-retrieving-image-from-sql-server-database-in-wpf/
+        //    //https://www.youtube.com/watch?v=XMhcF-zex6k
+        //    //https://www.youtube.com/watch?v=HIv1_P98Ne8&list=LL&index=2&t=182s
+        //}
+
+        //private void AudioPlay() // Chose the audio file from the computer
+        //{
+        //    OpenFileDialog openFile = new OpenFileDialog(); //create object for Filedialog
+
+        //    openFile.FileName = "";
+
+        //    openFile.Filter = "MP3 files|*.mp3"; // Filter only supported mp3 files
+
+        //    if (openFile.ShowDialog() == true) //Opens a dialog from the computer and if OK is clicked, the code in the if-statement will be executed
+        //    {
+        //        audioPath = openFile.FileName; // Set the filename path for the audio
+        //        mediaPlayer.Open(new Uri(audioPath)); //Open the audio from the filepath with the mediaplayer
+        //        mediaPlayer.Play(); // Play the audio with mediaplayer
+        //    }
+        //}
+        //private void AddImage(bool fileType, uint personID) //Click on the add button
+        //{
+        //    // Connection string to the database
+        //    //string connString = "Server=tcp:st4prj4.database.windows.net,1433;Initial Catalog=ST4PRJ4;Persist Security Info=False;User ID=azureuser;Password=Katrinebjerg123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"; //Connection string for the database
+
+        //    SqlConnection connection = new SqlConnection(connString);
+
+        //    connection.Open(); //Open the connection to the database
+
+        //    SqlCommand command = connection.CreateCommand();
+
+        //    // Convert image to byte array
+        //    byte[] data;
+
+        //    data = File.ReadAllBytes(imagePath); //Reads the content of the audio file and converting it to byte array
+
+        //    //Define image parameter
+        //    command.Parameters.AddWithValue("@image", data);
+
+        //    //query
+        //    command.CommandText = $"INSERT INTO Test_table(PersonID, Image) VALUES({personID}, @image)"; //Insert commandtext to database
+
+        //    if (command.ExecuteNonQuery() > 0) //Check if the command get executed to the database
+        //    {
+        //        MessageBox.Show("Billedet blev uploadet til database");
+        //    }
+        //    else
+        //    {
+        //        MessageBox.Show("Billedet blev ikke uploadet til database");
+        //    }
+        //    connection.Close(); // Close connection to the database
+
+        //}
 
     }
 }
