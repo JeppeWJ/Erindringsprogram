9fbaf68efd3c82c554c39e06ae7b94e0ac0ba5ab
Jeppe Walther Jensen <jepwaljen@gmail.com>
2022-04-21 09:01:58 +0200

    Metoder tilføjet til DataControl, men ikke implementeret

--- Presentation/DataControl.cs
+++ Presentation/DataControl.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DataAccess_2;
+using Logic_2._0.RelativeManagerClasses;
 
 namespace Presentation
 {
@@ -24,6 +25,27 @@ namespace Presentation
             int length = blob.Length;
             Blob = new byte[length];
             Blob = blob;
+        }
+
+        public List<RelativeDTO> Relatives { get; set; }
+        public void UpdateFile(bool fileType, uint id)
+        {
+           throw new NotImplementedException();
+        }
+
+        public void CreateProfile(RelativeDTO newProfile)
+        {
+           throw new NotImplementedException();
+        }
+
+        public void DeleteProfile(uint id)
+        {
+           throw new NotImplementedException();
+        }
+
+        public void EditProfile(RelativeDTO profile)
+        {
+           throw new NotImplementedException();
         }
     }
 }
