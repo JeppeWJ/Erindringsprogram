7ca7aa449bfec97cdb41cf58c4439aae0d861f10
soerenmehlsen <202000071@post.au.dk>
2022-04-22 20:30:35 +0200

    Linking combox with database is implemented and works.

--- Logic 2.0/RelativeManagerClasses/TestRelativeManager.cs
+++ Logic 2.0/RelativeManagerClasses/TestRelativeManager.cs
@@ -3,14 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DataAccess_2;
 
 namespace Logic_2._0.RelativeManagerClasses
 {
    public class TestRelativeManager : IRelativeManager
    {
       public List<RelativeDTO> Relatives { get; set; }
-      private DataAccess _dataAccess;
+      //private DataAccess _dataAccess;
 
       public TestRelativeManager()
       {
