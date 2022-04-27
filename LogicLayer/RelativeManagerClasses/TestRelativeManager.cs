1a1c2121bee769682cb3beb4cf33c1257832cd8e
Jeppe Walther Jensen <jepwaljen@gmail.com>
2022-04-27 09:46:44 +0200

    TestRelativeManager implementere nu fra IDataAccessObserver

--- LogicLayer/RelativeManagerClasses/TestRelativeManager.cs
+++ LogicLayer/RelativeManagerClasses/TestRelativeManager.cs
@@ -8,13 +8,35 @@ using DTOs;
 
 namespace LogicLayer.RelativeManagerClasses
 {
-   public class TestRelativeManager 
+   public class TestRelativeManager : IDataAccessObserver
     {
+       public void Update(RelativeDTO update, bool fileType)
+       {
+          throw new NotImplementedException();
+       }
 
+       public List<RelativeDTO> Relatives { get; set; }
+       public void UpdateFile(bool fileType, uint id)
+       {
+          throw new NotImplementedException();
+       }
 
-        public List<RelativeDTO> Relatives { get; set; }
+       public void CreateProfile(RelativeDTO newProfile)
+       {
+          throw new NotImplementedException();
+       }
 
-        //private DataAccess _dataAccess;
+       public void DeleteProfile(uint id)
+       {
+          throw new NotImplementedException();
+       }
+
+       public void EditProfile(RelativeDTO profile)
+       {
+          throw new NotImplementedException();
+       }
+
+       //private DataAccess _dataAccess;
 
       public TestRelativeManager()
       {
